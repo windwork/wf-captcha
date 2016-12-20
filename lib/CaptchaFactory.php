25cf7351e10b8e4e510933b483fc3412b8599ae8
erzh <cmpan@qq.com>
2016-12-20 16:00:43 +0800

    修改@copyright到2016

--- lib/CaptchaFactory.php
+++ lib/CaptchaFactory.php
@@ -4,7 +4,7 @@
  * 
  * 一个开源的PHP轻量级高效Web开发框架
  * 
- * @copyright   Copyright (c) 2008-2015 Windwork Team. (http://www.windwork.org)
+ * @copyright   Copyright (c) 2008-2016 Windwork Team. (http://www.windwork.org)
  * @license     http://opensource.org/licenses/MIT	MIT License
  */
 namespace wf\captcha;
