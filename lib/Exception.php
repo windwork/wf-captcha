11f279413955036312c2f62fc73eaf893a926474
erzh <cmpan@qq.com>
2017-06-23 14:06:24 +0800

    修改文件注释中的Slogan

--- lib/Exception.php
+++ lib/Exception.php
@@ -2,7 +2,7 @@
 /**
  * Windwork
  * 
- * 一个开源的PHP轻量级高效Web开发框架
+ * 一个用于快速开发高并发Web应用的轻量级PHP框架
  * 
  * @copyright Copyright (c) 2008-2017 Windwork Team. (http://www.windwork.org)
  * @license   http://opensource.org/licenses/MIT
